7a7f91df4e610d79f24e3e7a362e90e2dbc2c643
Xander <fehsenfeld.xander@gmail.com>
2026-01-18 16:45:25 -0800

    Add willapa and grays interactive view

--- app/layout.tsx
+++ app/layout.tsx
@@ -45,7 +45,7 @@ export default function RootLayout({
   children: React.ReactNode
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" suppressHydrationWarning>
       {Settings.gtmconnected && <GoogleTagManager gtmId={Settings.gtm} />}
       <body className={`${inter.variable} font-regular`}>
         <Providers>
