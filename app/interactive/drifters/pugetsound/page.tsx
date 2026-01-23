3af787a5a279a8332e3a1743681ae7a73839e0b0
Xander Fehsenfeld <xander@debian-desktop.net>
2026-01-23 11:30:49 -0800

    Remove unused landing page

--- app/interactive/drifters/pugetsound/page.tsx
+++ app/interactive/drifters/pugetsound/page.tsx
@@ -1,13 +1,9 @@
-import { notFound } from "next/navigation"
-
-import { Settings } from "@/types/settings"
-import { getDocument } from "@/lib/markdown"
 import { PageRoutes } from "@/lib/pageroutes"
 import { Separator } from "@/components/ui/separator"
 import { Typography } from "@/components/ui/typography"
 import { ArticleBreadcrumb } from "@/components/article/breadcrumb"
 import { Pagination } from "@/components/article/pagination"
-import { TableOfContents } from "@/components/toc"
+
 import DriftersPugetSound from "./DriftersPugetSound"
 
 interface PageProps {
@@ -19,31 +15,36 @@ export default async function Pages({ params }: PageProps) {
   const result = await params
   const pathName = slug.join("/")
 
-  console.log('pathname', result)
-
+  console.log("pathname", result)
 
   return (
     <div className="flex items-start gap-10">
       <section className="flex-3">
-        <ArticleBreadcrumb paths={['drifters','pugetsound']} prefix={'/interactive'} />
+        <ArticleBreadcrumb
+          paths={["drifters", "pugetsound"]}
+          prefix={"/interactive"}
+        />
         <div className="space-y-4">
-          <h1 className="text-3xl font-semibold">Puget Sound: Drifter Tracks</h1>
-          <p className="text-sm">The map plot shows tracks from simulated surface drifter tracks over three days from the most recent LiveOcean daily forecast.</p>
+          <h1 className="text-3xl font-semibold">
+            Puget Sound: Drifter Tracks
+          </h1>
+          <p className="text-sm">
+            The map plot shows tracks from simulated surface drifter tracks over
+            three days from the most recent LiveOcean daily forecast.
+          </p>
           <Separator />
         </div>
 
-
         <Typography>
-          <section><DriftersPugetSound/></section>
+          <section>
+            <DriftersPugetSound />
+          </section>
           <Pagination pathname={pathName} />
         </Typography>
       </section>
-
     </div>
   )
 }
-
-
 
 export function generateStaticParams() {
   return PageRoutes.filter((item) => item.href).map((item) => ({
--- app/interactive/drifters/willapaandgrays/page.tsx
+++ app/interactive/drifters/willapaandgrays/page.tsx
@@ -1,13 +1,10 @@
-import { notFound } from "next/navigation"
 
-import { Settings } from "@/types/settings"
-import { getDocument } from "@/lib/markdown"
 import { PageRoutes } from "@/lib/pageroutes"
 import { Separator } from "@/components/ui/separator"
 import { Typography } from "@/components/ui/typography"
 import { ArticleBreadcrumb } from "@/components/article/breadcrumb"
 import { Pagination } from "@/components/article/pagination"
-import { TableOfContents } from "@/components/toc"
+
 import DriftersWillapaAndGrays from "./DriftersWillapaAndGrays"
 
 interface PageProps {
@@ -18,27 +15,33 @@ export default async function Pages({ params }: PageProps) {
   const { slug = [] } = await params
   const pathName = slug.join("/")
 
-
-
   return (
     <div className="flex items-start gap-10">
       <section className="flex-3">
-        <ArticleBreadcrumb paths={['drifters','willapaandgrays']} prefix={'/interactive'} />
+        <ArticleBreadcrumb
+          paths={["drifters", "willapaandgrays"]}
+          prefix={"/interactive"}
+        />
         <div className="space-y-4">
-          <h1 className="text-3xl font-semibold">Willapa Bay & Grays Harbor Drifter Tracks</h1>
-          <p className="text-sm">The map plot shows tracks from simulated drifter tracks over three days from the most recent LiveOcean daily forecast. </p>
+          <h1 className="text-3xl font-semibold">
+            Willapa Bay & Grays Harbor Drifter Tracks
+          </h1>
+          <p className="text-sm">
+            The map plot shows tracks from simulated drifter tracks over three
+            days from the most recent LiveOcean daily forecast.{" "}
+          </p>
           <Separator />
         </div>
         <Typography>
-          <section><DriftersWillapaAndGrays/></section>
+          <section>
+            <DriftersWillapaAndGrays />
+          </section>
           <Pagination pathname={pathName} />
         </Typography>
       </section>
-
     </div>
   )
 }
-
 
 export function generateStaticParams() {
   return PageRoutes.filter((item) => item.href).map((item) => ({
