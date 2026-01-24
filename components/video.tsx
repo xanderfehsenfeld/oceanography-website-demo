099aac0942fdc79a7e1c90d03b15d42464e7efec
Xander Fehsenfeld <xander@debian-desktop.net>
2026-01-24 08:09:21 -0800

    Add width and height to videos

--- components/video.tsx
+++ components/video.tsx
@@ -1,9 +1,9 @@
 import * as React from "react"
 
-
-
-
-function Video({ src, ...props }: React.ComponentProps<"video">) {
+function Video({
+  src,
+  ...props
+}: React.ComponentProps<"video"> & { width: number; height: number }) {
   return (
     <video controls loop autoPlay className="not-prose" {...props}>
       <source src={src as string} type="video/mp4" />
