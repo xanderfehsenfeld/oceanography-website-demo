6d9c4cc4045867358729e44d3ddb42068f9d8b9c
Xander Fehsenfeld <xander@debian-desktop.net>
2026-01-26 09:54:35 -0800

    Add map view with zoom and bounds locked

--- app/interactive/observationsviewer/ObservationsViewer.tsx
+++ app/interactive/observationsviewer/ObservationsViewer.tsx
@@ -1,7 +1,10 @@
 "use client"
 
-import React, { useEffect } from "react"
+import React, { useEffect, useState } from "react"
+import * as d3 from "d3"
 import L from "leaflet"
+
+import { Slider } from "@/components/slider"
 
 import geography from "./ne_10m_coastline.json"
 import marinePolys from "./ne_10m_geography_marine_polys.json"
@@ -46,36 +49,91 @@ const mapSources: { [name: string]: ISource } = {
       '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>',
   },
 }
+
+function projectPoint(x: number, y: number) {
+  var point = map.latLngToLayerPoint(new L.LatLng(y, x))
+  this.stream.point(point.x, point.y)
+}
+
 const initializeMap = () => {
   map?.remove()
   deleteAllChildNodes("map")
 
-  map = L.map("map").setView([48, -124], 7)
+  map = L.map("map", {
+    zoomControl: false,
+    maxZoom: 8,
+    minZoom: 8,
+  }).setView([48, -124], 8)
 
-  L.tileLayer(mapSources.darkMatterNoLabels.url, {
-    attribution: mapSources.darkMatterNoLabels.attribution,
-    maxZoom: 10,
-    minZoom: 7,
+  L.tileLayer(mapSources.voyagerNoLabels.url, {
+    attribution: mapSources.voyagerNoLabels.attribution,
   }).addTo(map)
 
-  const geoJSONLayer = L.geoJSON().addTo(map)
-
   map.setMaxBounds(map.getBounds())
+
+  var svg = d3.select(map.getPanes().overlayPane).append("svg")
+  const g = svg.append("g").attr("class", "leaflet-zoom-hide")
+
+  const transform = d3.geoTransform({ point: projectPoint })
+  const path = d3.geoPath().projection(transform)
+
+  var feature = g
+    .selectAll("path")
+    .data(marinePolys.features)
+    .enter()
+    .append("path")
+
+  feature.attr("d", path)
+
+  function reset() {
+    var bounds = path.bounds(marinePolys),
+      topLeft = bounds[0],
+      bottomRight = bounds[1]
+
+    console.log("bounds", topLeft, bottomRight)
+
+    const width = bottomRight[0] - topLeft[0]
+
+    const height = bottomRight[1] - topLeft[1]
+
+    console.log(width, height)
+
+    svg
+      .attr("width", bottomRight[0] - topLeft[0])
+      .attr("height", bottomRight[1] - topLeft[1])
+      .style("left", topLeft[0] + "px")
+      .style("top", topLeft[1] + "px")
+
+    g.attr("transform", "translate(" + -topLeft[0] + "," + -topLeft[1] + ")")
+  }
+
+  reset()
 
   // geoJSONLayer.addData(marinePolys)
 }
 
 function MapChart() {
   useEffect(initializeMap)
+
   return (
-    <div className="not-prose">
+    <div className="not-prose flex flex-col gap-2">
       <link
         rel="stylesheet"
         href="https://unpkg.com/leaflet@1.9.4/dist/leaflet.css"
         integrity="sha256-p4NxAoJBhIIN+hmNHrzRCf9tD/miZyoHS5obTRR9BMY="
         crossOrigin=""
       />{" "}
-      <div id="map" className="h-[360px]"></div>
+      <div key={"key"} id="map" className="h-[540px]"></div>
+      <link href="/scripts/jspm/style1.css" rel="stylesheet" type="text/css" />
+      <Slider
+        onChange={(input) => {
+          // setSliderValue(input.target.valueAsNumber)//
+        }}
+        min={0}
+        max={16}
+        className="slider"
+        id="myRange"
+      />
     </div>
   )
 }
