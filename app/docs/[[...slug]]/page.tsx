2b0deb509f21396bd22650f8997c99a72ea3e7ae
Xander Fehsenfeld <xander@debian-desktop.net>
2026-04-30 11:38:12 -0700

    Add last edited sections

--- app/docs/[[...slug]]/page.tsx
+++ app/docs/[[...slug]]/page.tsx
@@ -3,6 +3,7 @@ import { notFound } from "next/navigation"
 import { Settings } from "@/types/settings"
 import { getDocument } from "@/lib/markdown"
 import { PageRoutes } from "@/lib/pageroutes"
+import { LastEdited } from "@/components/ui/last-edited"
 import { Separator } from "@/components/ui/separator"
 import { Typography } from "@/components/ui/typography"
 import { ArticleBreadcrumb } from "@/components/article/breadcrumb"
@@ -23,7 +24,7 @@ export default async function Pages({ params }: PageProps) {
 
   if (!res) notFound()
 
-  const { frontmatter, content, tocs } = res
+  const { frontmatter, content, tocs, lastUpdated } = res
 
   return (
     <div className="flex items-start gap-10">
@@ -33,9 +34,12 @@ export default async function Pages({ params }: PageProps) {
           <h1 className="text-3xl font-semibold">{frontmatter.title}</h1>
           <p className="text-sm">{frontmatter.description}</p>
           <Separator />
+          {lastUpdated && <LastEdited lastEdited={lastUpdated} />}
         </div>
         <Typography>
           <section>{content}</section>
+          {lastUpdated && <LastEdited lastEdited={lastUpdated} />}
+
           <Pagination pathname={pathName} />
         </Typography>
       </section>
@@ -43,6 +47,7 @@ export default async function Pages({ params }: PageProps) {
         tocs={{ tocs }}
         pathName={pathName}
         frontmatter={frontmatter}
+        lastUpdated={lastUpdated ? new Date(lastUpdated) : undefined}
       />
     </div>
   )
---
+++ components/ui/last-edited.tsx
@@ -0,0 +1,23 @@
+import clsx from "clsx"
+
+const lastEditedFormat = new Intl.DateTimeFormat("en-US", {
+  timeStyle: "short",
+  dateStyle: "medium",
+})
+
+export const LastEdited = ({
+  lastEdited,
+  className,
+}: {
+  className?: string
+  lastEdited: string
+}) => (
+  <div
+    className={clsx(
+      "hidden items-center text-xs text-nowrap text-ellipsis italic md:block",
+      className
+    )}
+  >
+    Last edited {lastEditedFormat.format(new Date(lastEdited))}
+  </div>
+)
