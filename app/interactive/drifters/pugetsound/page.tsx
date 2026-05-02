4682d46a9630c1c3252a1489fc003b38dfc94718
Xander <fehsenfeld.xander@gmail.com>
2026-05-02 07:57:52 -0700

    Remove prefetch causing error

--- app/interactive/drifters/pugetsound/page.tsx
+++ app/interactive/drifters/pugetsound/page.tsx
@@ -1,16 +1,12 @@
-import { SWRConfig } from "swr"
-
 import { Separator } from "@/components/ui/separator"
 import { Typography } from "@/components/ui/typography"
 import { ArticleBreadcrumb } from "@/components/article/breadcrumb"
 import { Pagination } from "@/components/article/pagination"
 
 import { fetchPoints, fetchTimes } from "../../fetchData"
-import DriftersPugetSound, { dataFilenames } from "./DriftersPugetSound"
+import DriftersPugetSound from "./DriftersPugetSound"
 
 export default async function Pages() {
-  const tracksPromise = fetchPoints(dataFilenames.tracks)
-  const timesPromise = fetchTimes(dataFilenames.times)
   return (
     <section className="flex-3">
       <ArticleBreadcrumb
@@ -28,48 +24,38 @@ export default async function Pages() {
 
       <Typography>
         <section className="pb-4">
-          <SWRConfig
-            value={{
-              fallback: {
-                // Pass the promises to client components.
-                [dataFilenames.tracks]: tracksPromise,
-                [dataFilenames.times]: timesPromise,
-              },
-            }}
-          >
-            <DriftersPugetSound>
-              <h3>Puget Sound Drifter Tracks</h3>
+          <DriftersPugetSound>
+            <h3>Puget Sound Drifter Tracks</h3>
 
-              <p>
-                The map plot shows tracks from simulated surface drifter tracks
-                over three days from the most recent LiveOcean daily forecast.
-                At the start time you can see the initial drifter release
-                locations as blue dots. Using the "Time Slider" you can see
-                where each particle goes in time. Use the playback buttons to
-                play the forecast at different speeds.
-              </p>
+            <p>
+              The map plot shows tracks from simulated surface drifter tracks
+              over three days from the most recent LiveOcean daily forecast. At
+              the start time you can see the initial drifter release locations
+              as blue dots. Using the "Time Slider" you can see where each
+              particle goes in time. Use the playback buttons to play the
+              forecast at different speeds.
+            </p>
 
-              <h4>Selecting a single drifter</h4>
+            <h4>Selecting a single drifter</h4>
 
-              <p>
-                If you click on a drifter, a green line will appear showing its
-                path over the length of the forecast. Use this feature to
-                determine the origin and destination of any single drifter.
-              </p>
+            <p>
+              If you click on a drifter, a green line will appear showing its
+              path over the length of the forecast. Use this feature to
+              determine the origin and destination of any single drifter.
+            </p>
 
-              <h4>Selecting and tracking groups of drifters</h4>
+            <h4>Selecting and tracking groups of drifters</h4>
 
-              <p>
-                If you click on the map (not a single drifter), all the drifters
-                within a certain distance will turn red. Increase or decrease
-                the area of selection by zooming in or out of the map. By
-                selecting different groups of particles at different times you
-                can explore questions such as: Where do all the particles from
-                one place go? or Where did all the particles that ended up in
-                some place come from?
-              </p>
-            </DriftersPugetSound>
-          </SWRConfig>
+            <p>
+              If you click on the map (not a single drifter), all the drifters
+              within a certain distance will turn red. Increase or decrease the
+              area of selection by zooming in or out of the map. By selecting
+              different groups of particles at different times you can explore
+              questions such as: Where do all the particles from one place go?
+              or Where did all the particles that ended up in some place come
+              from?
+            </p>
+          </DriftersPugetSound>
         </section>
         <Pagination pathname={"drifters/pugetsound"} prefix="interactive" />
       </Typography>
