ba5f5d0ea1436be03e46fd8e2d5ec63ef1d046a1
Xander <fehsenfeld.xander@gmail.com>
2026-07-01 12:39:17 -0700

    Add more tests

--- app/interactive/__tests__/fetchData.test.ts
+++ app/interactive/__tests__/fetchData.test.ts
@@ -2,7 +2,7 @@ import { describe } from "node:test"
 
 import { expect, test } from "vitest"
 
-import { fetchPoints } from "../fetchData"
+import { fetchPoints, fetchTimes } from "../fetchData"
 
 describe("fetchData", async () => {
   test("interpolates more data", async () => {
@@ -15,5 +15,22 @@ describe("fetchData", async () => {
     const fetchedPoints = await fetchPoints("PS_tracks.json")
 
     expect(fetchedPoints.length).toBeGreaterThan(originalData[0].x.length)
+  })
+})
+
+describe("fetchTimes", async () => {
+  const originalData = (await (
+    await fetch("/api/liveocean-web/PS_times.json")
+  ).json()) as any
+  const fetchedTimes = await fetchTimes("PS_times.json")
+  test("interpolates more values", async () => {
+    await expect(originalData[0].t).toHaveLength(73)
+
+    expect(fetchedTimes.length).toBeGreaterThan(originalData[0].t.length)
+  })
+  test("outputs date string", async () => {
+    const [firstEntry] = fetchedTimes
+
+    expect(firstEntry).toEqual("Jun 30, 2026, 5:00 PM")
   })
 })
